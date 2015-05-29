cb58b5ea66fb9e4fb681595087c6b0d982ddf443
Tyler Silva <tylerbsilva@gmail.com>
2015-05-29 13:07:41 -0500

    switched to list

--- apps/app.js
+++ apps/app.js
@@ -8,20 +8,26 @@ $(function(){
 
 function getRequest(searchTerm){
   var params = {
-    s: searchTerm,
-    r: 'json'
+    part: 'snippet',
+    key: 'AIzaSyAW0q3d5W5bF9rG-9oP4mitrTdEnM8L3R4',
+    q: searchTerm
   };
-  url = 'http://www.omdbapi.com';
+  url = 'https://www.googleapis.com/youtube/v3/search';
 
   $.getJSON(url, params, function(data){
-    showResults(data.Search);
+    showResults(data.items);
   });
 }
 
 function showResults(results){
   var html = "";
   $.each(results, function(index,value){
-    html += '<p>' + value.Title + '</p>';
+    html += '<li>';
+    html += '<a href="//youtube.com/watch?v=' + value.id.videoId + '">';
+    html += '<img src="' + value.snippet.thumbnails.medium.url + '"" />';
+    html += '<h3>' + value.snippet.title + '</h3>';
+    html += '</a></li>';
+    console.log(value);
   });
   $('#search-results').html(html);
 }
